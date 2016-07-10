71a7b8a30535a8816ff88f3979431cfbc2eadf1f
lourdesVillca <lourdes.villca@fundacion-jala.org>
2016-07-10 19:56:17 -0400

    Epic - API testing (GET,POST,PUT,DELETE)

---
+++ features/step_definitions/epic_steps/epic.rb
@@ -0,0 +1,20 @@
+
+When(/^I send a epic (GET) request to (.*?)$/) do |method, end_point|
+  http_request = @client.get_request(method, end_point)
+  @http_response = @client.execute_request(@http_connection, http_request)
+  @http_response.body
+
+end
+When(/^I send an epic (PUT|POST) request to (.*?) with json$/) do |method, end_point, json_text|
+  http_request = @client.get_request(method, end_point)
+  http_request.body = json_text
+  @http_response = @client.execute_request(@http_connection, http_request)
+  @last_json = @http_response.body
+end
+
+When(/^I send an epic (DELETE) request to (.*?)$/) do |method, end_point|
+  http_request = @client.get_request(method, end_point)
+  @http_response = @client.execute_request(@http_connection, http_request)
+  @last_json = @http_response.body
+
+end
