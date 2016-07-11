d17cc1fa34820a1482b0582ec1f4d7de47df23f0
lourdesVillca <lourdes.villca@fundacion-jala.org>
2016-07-11 17:05:36 -0400

    Solve problems with ids

--- features/step_definitions/epic_steps/epic.rb
+++ features/step_definitions/epic_steps/epic.rb
@@ -3,7 +3,6 @@ When(/^I send a epic (GET) request to (.*?)$/) do |method, end_point|
   http_request = @client.get_request(method, end_point)
   @http_response = @client.execute_request(@http_connection, http_request)
 
-
 end
 When(/^I send an epic (PUT|POST) request to (.*?) with json$/) do |method, end_point, json_text|
   http_request = @client.get_request(method, end_point)
