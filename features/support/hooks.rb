05d5b0c0758a727616d8c5583c382f716dcf2493
angy113 <rockstaradjm@gmail.com>
2016-07-20 17:12:43 -0400

    Projects

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -5,7 +5,7 @@ Before('@create_project') do
   client = ApiRestClient.new
   #create the project
   json_param = {:name => "General_project01"}
-  $project = ProjectRequest.create_project(client,"POST",json_param.to_json)
+  _,$project = ProjectRequest.create_project(client,"POST",json_param.to_json)
 
   $array_response_epic_json = Array.new
   $array_send_json = Array.new
