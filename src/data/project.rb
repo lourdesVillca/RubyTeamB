fc6c532b97fadce972e2e32d04b717888c2d1c42
angy113 <rockstaradjm@gmail.com>
2016-07-21 14:40:31 -0400

    Projects final

--- src/data/project.rb
+++ src/data/project.rb
@@ -31,8 +31,6 @@ class Project < BaseClassForDataClasses
                 :version,
                 :week_start_day
 
-  ERROR = {:unfound_resource => "The object you tried to access could not be found.  It may have been removed by another user, you may be using the ID of another object type, or you may be trying to access a sub-resource at the wrong point in a tree."}
-
   def initialize(values = {})
     super(values)
   end
